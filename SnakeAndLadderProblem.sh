e46b47f0cce484b46f4317680d562c11ef4075f0
dbhide <dhanashree.bhide3@gmail.com>
2020-01-31 18:00:58 +0530

    ADDED: Function to check for among options NoPlay, Ladder or Snake

--- SnakeAndLadderProblem.sh
+++ SnakeAndLadderProblem.sh
@@ -5,5 +5,24 @@ echo "Welcome to Snake and Ladder Simulator"
 #variable
 startPosition=0
 
-dieRoll=$((RANDOM%6+1))
+function dieRoll() {
+	dice=$((RANDOM%6+1))
+	echo $dice
+}
+dieRoll
 
+function optionCheck() {
+	options=$((RANDOM%3+1))
+	case $options in
+		1)
+			startPosition=$startPosition
+			;;
+		2)
+			startPosition=$((startPosition+$(dieRoll)))
+			;;
+		3)
+			startPosition=$((startPosition-$(dieRoll)))
+			;;
+	esac
+}
+optionCheck
